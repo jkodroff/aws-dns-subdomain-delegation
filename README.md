c8104edc30f950688b85c5b7607e5a4e29a09e1d
Josh Kodroff <josh@pulumi.com>
2024-07-18 16:44:50 -0400

    Make the DNS setup code more portable.

--- README.md
+++ README.md
@@ -8,3 +8,56 @@ Demo using Pulumi for delegating subdomains for a hosted zone in Route 53:
   - This stack is presumed to execute in the same Pulumi org as `domain-account`.
 - `subdomain-account-workload` contains a simple S3 static site configured to be at the `www.` sub-subdomain of the delegated subdomain, e.g. `www.foo.example.com`:
   - This stack is presumed to execute in the same Pulumi order as `subdomain-account`.
+
+## Instructions for Delegating a Subdomain
+
+In the `subdomain-account` directory:
+
+1. Set the FQDN of the subdomain, e.g.:
+
+    ```bash
+    # First name, handle, username, etc.
+    pulumi config set subdomainFqdn yourname.pulumi-workshops.com
+    ```
+
+1. Set the assumable role ARN (get this from the instructors), e.g.
+
+    ```bash
+    pulumi config set parentZoneRoleArn arn:aws:iam::123456789012:role/role-name
+    ```
+
+1. Run the Pulumi program:
+
+    ```bash
+    pulumi up
+    ```
+
+1. Optionally, you can verify the configuration of your subdomain by deploying a simple S3 site:
+
+    ```bash
+    cd ../subdomain-account-workload
+    pulumi up -y
+    ```
+
+    Test by running the following command:
+
+    ```bash
+    curl $(pulumi stack output url)
+    ```
+
+    You should see the following response:
+
+    ```text
+    <html>
+
+    <head>
+      <meta charset="UTF-8">
+      <title>It works!</title>
+    </head>
+
+    <body>
+      <h1>It works!</h1>
+    </body>
+
+    </html>
+    ```
--- domain-account/index.ts
+++ domain-account/index.ts
@@ -2,20 +2,22 @@ import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
 
 const config = new pulumi.Config();
-export const hostedZoneId = config.require("hostedZoneId");
+export const hostedZoneName = config.require("hostedZoneName");
 
 // Verify that the hosted zone actually exists:
-aws.route53.Zone.get("existing-hosted-zone", hostedZoneId);
+const zone = aws.route53.getZoneOutput({
+  name: hostedZoneName,
+});
 
-// Define the AWS account IDs that will assume the role
-const trustedAccountIds = [
-  "616138583583", // pulumi-dev-sandbox
-];
+interface TrustedAccountIds {
+  data: string[];
+}
 
+const trustedAccountIds = config.requireObject<TrustedAccountIds>("trustedAccountIds");
 
 const assumeRolePolicy = {
   Version: "2012-10-17",
-  Statement: trustedAccountIds.map(accountId => ({
+  Statement: trustedAccountIds?.data.map(accountId => ({
     Effect: "Allow",
     Principal: {
       AWS: `arn:aws:iam::${accountId}:root`,
@@ -33,38 +35,44 @@ const assumeRolePolicy = {
 };
 
 const role = new aws.iam.Role("delegation-role", {
+  name: "zero-to-hero-subdomain",
   assumeRolePolicy: JSON.stringify(assumeRolePolicy),
 });
 
-const policyDocument = {
-  Version: "2012-10-17",
-  Statement: [
-    {
-      Effect: "Allow",
-      Action: [
-        "route53:GetHostedZone",
-        "route53:ChangeResourceRecordSets",
-        "route53:GetChange",
-        "route53:ListResourceRecordSets",
-      ],
-      Resource: `arn:aws:route53:::hostedzone/${hostedZoneId}`,
-    },
-    // It may be possible to make this permission less broad. This appears to be
-    // necessary so that the AWS provider only returns after the records have
-    // actually been created:
-    {
-      Effect: "Allow",
-      Action: [
-        "route53:GetChange",
-      ],
-      Resource: `*`,
-    },
-  ],
-};
+const policyDocument = zone.zoneId.apply(zoneId => {
+  const policy = {
+    Version: "2012-10-17",
+    Statement: [
+      {
+        Effect: "Allow",
+        Action: [
+          "route53:GetHostedZone",
+          "route53:ChangeResourceRecordSets",
+          "route53:GetChange",
+          "route53:ListResourceRecordSets",
+        ],
+        Resource: `arn:aws:route53:::hostedzone/${zoneId}`,
+      },
+      // It may be possible to make this permission less broad. This appears to be
+      // necessary so that the AWS provider only returns after the records have
+      // actually been created:
+      {
+        Effect: "Allow",
+        Action: [
+          "route53:GetChange",
+          "route53:ListHostedZones",
+          "route53:ListTagsForResource",
+        ],
+        Resource: `*`,
+      },
+    ],
+  };
+  return JSON.stringify(policy);
+});
 
-new aws.iam.RolePolicy("delegationRolePolicy", {
+new aws.iam.RolePolicy("delegation-role-policy", {
   role: role.id,
-  policy: JSON.stringify(policyDocument),
+  policy: policyDocument,
 });
 
 export const roleArn = role.arn;
